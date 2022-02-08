8cbf9e86a3002baedff101263d9ea318ec1fc5a3
djdu4496 <djdu4496@gmail.com>
2022-02-08 20:42:44 +0900

    feat : index.js import & clock 기능 추가

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector('.clock');
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`
+  const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`
+  clock.innerText = `${hours}:${minutes}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
+
